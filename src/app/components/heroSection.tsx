978f99cf98ad8a22a5d2bbabe2b8d32c1a80bd4b
Piyas Ahmed <apiyas761@gmail.com>
2026-09-24 13:58:36 +0600

    add Responsive Code

--- src/app/components/heroSection.tsx
+++ src/app/components/heroSection.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const HeroSection = () => {
   return (
-    <div className="  h-112.5 bg-[#ffffff1c] mt-15 p-10 rounded-3xl grid grid-cols-2 items-center container mx-auto gap-5  ">
+    <div className=" h-auto md:h-120.5 bg-[#ffffff1c] mt-15 p-10 rounded-3xl grid grid-cols-1 md:grid-cols-2 items-center container mx-auto gap-5  ">
       <div className=" text-white p-4 ">
         <p className="text-[14px] font-bold text-[#C2F800]">WORKOUT LIBRARY</p>
         <h1 className="text-5xl font-bold mb-4 leading-15 my-4 tracking-[-2px]">
@@ -10,7 +10,7 @@ const HeroSection = () => {
         </h1>
         <p className="text-lg">
           FitLog is a dark, no-nonsense gym companion: pick a lift, lock it into
-          today's plan, and watch the week's work add up.
+          today&apos;s plan, and watch the week&apos;s work add up.
         </p>
 
         <button className="mt-4  bg-[#C2F800] text-black font-bold px-4 py-2 rounded-lg text-[16px] cursor-pointer">
